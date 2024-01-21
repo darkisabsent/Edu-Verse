0f16d6f650f2c57aaf1643f27c91ccb362597785
selim thabet <thabetselim06@gmail.com>
2024-01-21 14:03:35 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Virtual Classroom',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The AI Teacher',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'The offline mode',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Virtual trips',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Simulations',
+  },
+];
+
+export const startingFeatures = [
+  'Accessible in multiple languages',
+  'Safe and comfortable',
+  'Recyclable material and with a guarantee',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Vision',
+    subtitle:
+        'Edu-Verse envisions a world where education transcends boundaries. We are passionate about harnessing the potential of Virtual Reality (VR) to create a dynamic and inclusive learning environment that caters to the diverse needs of learners worldwide.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Commitment',
+    subtitle:
+        "Edu-Verse is more than just a platform; it's a commitment to the transformative power of education. We are dedicated to Inclusivity, Quality and Continuous Improvement: Join us on this exciting journey as we redefine education for the digital age. ",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Immersive Learning Experiences',
+    subtitle:
+        "Step into a world of possibilities with our immersive VR learning experiences. Whether you're exploring ancient civilizations, conducting virtual science experiments, or mastering new languages",
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Personalized AI Tutoring',
+    subtitle:
+        'Our cutting-edge AI tutoring system provides real-time feedback and personalized learning journeys. Edu-Verse adapts to individual learning styles, ensuring that every student receives the support they need to succeed.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Innovation in Education',
+    subtitle:
+        ' Edu-Verse is at the forefront of innovation in education. We strive to improve access to quality education, enhance teaching methods, and prepare learners for the challenges and opportunities of the future job market.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
